4830d9b97b9137795211e4828cdf31945bd0bfef
Joe Foti <jfoti@4238.local>
2018-06-26 14:14:18 -0400

    Forgot to uncomment something

--- public/scripts/homepageRefresher.js
+++ public/scripts/homepageRefresher.js
@@ -97,67 +97,67 @@ $(document).ready(function(){
     });
 
 
-  // setInterval(function(){
-  //   $.ajax({
-  //     url:`../data/${tempLogin}_favoriteStopTimes.json`,
-  //     success: function(result) {
-  //       console.log('hello timeout!');
-  //       var result = result['favorites'];
-  //
-  //       let html = '';
-  //       for (let i=0;i < result.length;i++) {
-  //         console.log('checking result '+i)
-  //         if (!result[i]['station']) {
-  //           console.log('no station');
-  //           html += '<h5 class="error-msg">Uh-oh! No data for this stop.</h5>'
-  //           singles[i].innerHTML = html;
-  //           html = ''
-  //           continue;
-  //         }
-  //          html += `<ul class="time-container uptown"><h3 class="station-type">Northbound</h3>`
-  //          if (!result[i]['station'][0]) {
-  //            console.log('no northbound');
-  //            html += '<h5 class="error-msg">No trains at the northbound station</h5></ul>'
-  //            singles[i].innerHTML = html;
-  //            html = ''
-  //            continue;
-  //          }
-  //
-  //         for (let j=0; j<3; j++) {
-  //           if (!result[i]['station'][0]['est'][j]) continue;
-  //           html += `<li class="time-item line-type-${result[i]['station'][0]['est'][j][1]}"><span class="time-item-line">${result[i]['station'][0]['est'][j][1]}</span>${result[i]['station'][0]['est'][j][0]} Minutes</li>`
-  //         }
-  //           html += `</ul>`
-  //
-  //           html += `<ul class="time-container downtown"><h3 class="station-type">Southbound</h3>`
-  //
-  //         if (!result[i]['station'][1]) {
-  //           console.log('no soutbound');
-  //
-  //           html += '<h5 class="error-msg">No trains at the southbound station</h5>'
-  //           singles[i].innerHTML = html;
-  //           html = ''
-  //           continue;
-  //         }
-  //
-  //         for (let j=0; j<3; j++) {
-  //           if (!result[i]['station'][1]['est'][j]) continue;
-  //           html += `<li class="time-item line-type-${result[i]['station'][1]['est'][j][1]}"><span class="time-item-line">${result[i]['station'][1]['est'][j][1]}</span>${result[i]['station'][1]['est'][j][0]} Minutes</li>`
-  //         }
-  //           html += `</ul>`
-  //         // $(singles).eq(i).innerHTML(html);
-  //
-  //       singles[i].innerHTML = html;
-  //       html = '';
-  //       getLastRefresh(i);
-  //       }
-  //     },
-  //     failure: function(err) {
-  //       console.log('something went wrong!');
-  //       console.log(err);
-  //     }
-  //   });
-  // },15000);
+  setInterval(function(){
+    $.ajax({
+      url:`../data/${tempLogin}_favoriteStopTimes.json`,
+      success: function(result) {
+        console.log('hello timeout!');
+        var result = result['favorites'];
+
+        let html = '';
+        for (let i=0;i < result.length;i++) {
+          console.log('checking result '+i)
+          if (!result[i]['station']) {
+            console.log('no station');
+            html += '<h5 class="error-msg">Uh-oh! No data for this stop.</h5>'
+            singles[i].innerHTML = html;
+            html = ''
+            continue;
+          }
+           html += `<ul class="time-container uptown"><h3 class="station-type">Northbound</h3>`
+           if (!result[i]['station'][0]) {
+             console.log('no northbound');
+             html += '<h5 class="error-msg">No trains at the northbound station</h5></ul>'
+             singles[i].innerHTML = html;
+             html = ''
+             continue;
+           }
+
+          for (let j=0; j<3; j++) {
+            if (!result[i]['station'][0]['est'][j]) continue;
+            html += `<li class="time-item line-type-${result[i]['station'][0]['est'][j][1]}"><span class="time-item-line">${result[i]['station'][0]['est'][j][1]}</span>${result[i]['station'][0]['est'][j][0]} Minutes</li>`
+          }
+            html += `</ul>`
+
+            html += `<ul class="time-container downtown"><h3 class="station-type">Southbound</h3>`
+
+          if (!result[i]['station'][1]) {
+            console.log('no soutbound');
+
+            html += '<h5 class="error-msg">No trains at the southbound station</h5>'
+            singles[i].innerHTML = html;
+            html = ''
+            continue;
+          }
+
+          for (let j=0; j<3; j++) {
+            if (!result[i]['station'][1]['est'][j]) continue;
+            html += `<li class="time-item line-type-${result[i]['station'][1]['est'][j][1]}"><span class="time-item-line">${result[i]['station'][1]['est'][j][1]}</span>${result[i]['station'][1]['est'][j][0]} Minutes</li>`
+          }
+            html += `</ul>`
+          // $(singles).eq(i).innerHTML(html);
+
+        singles[i].innerHTML = html;
+        html = '';
+        getLastRefresh(i);
+        }
+      },
+      failure: function(err) {
+        console.log('something went wrong!');
+        console.log(err);
+      }
+    });
+  },15000);
 
 
 }
