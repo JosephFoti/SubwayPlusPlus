dadfb5046779a6cac614c82cd7ffb514207b4abf
Joseph Foti <eljofo@gmail.com>
2018-07-08 16:31:26 -0400

    Ajax route adjustment

--- public/scripts/singleStatusCheck.js
+++ public/scripts/singleStatusCheck.js
@@ -3,9 +3,9 @@ console.log('are you single?');
 let line = $('.subtitle').text()[0];
 
 $.ajax({
-  url:`/data/${line}_statusReport.json`,
+  url:`../data/${line}_statusReport.json`,
   success: report=>{
-    
+
     $('.stop-status-type').text('SERVICE STATUS: ' + report.status);
     $('.stop-status-text').html(report.text);
 
